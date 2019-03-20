8bb9b916fca1175bb0d2898cde17dcb90f7705ef
eslammedhat22 <eslammedhat35@gmail.com>
2019-03-20 13:38:21 +0200

    .

---
+++ Compiler Scanner C#/Scanner/Scanner/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Scanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.Browse = new System.Windows.Forms.Button();
+            this.Run = new System.Windows.Forms.Button();
+            this.filePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.error = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Browse
+            // 
+            this.Browse.BackColor = System.Drawing.Color.LavenderBlush;
+            this.Browse.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.Browse.Location = new System.Drawing.Point(320, 56);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(77, 29);
+            this.Browse.TabIndex = 0;
+            this.Browse.Text = "Browse";
+            this.Browse.UseVisualStyleBackColor = false;
+            this.Browse.Click += new System.EventHandler(this.Browse_Click);
+            // 
+            // Run
+            // 
+            this.Run.BackColor = System.Drawing.Color.LimeGreen;
+            this.Run.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.Run.Location = new System.Drawing.Point(426, 57);
+            this.Run.Name = "Run";
+            this.Run.Size = new System.Drawing.Size(79, 28);
+            this.Run.TabIndex = 1;
+            this.Run.Text = "Run";
+            this.Run.UseVisualStyleBackColor = false;
+            this.Run.Click += new System.EventHandler(this.Run_Click);
+            // 
+            // filePath
+            // 
+            this.filePath.Location = new System.Drawing.Point(12, 57);
+            this.filePath.Name = "filePath";
+            this.filePath.Size = new System.Drawing.Size(276, 22);
+            this.filePath.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Info;
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "File Path";
+            // 
+            // error
+            // 
+            this.error.AutoSize = true;
+            this.error.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.error.ForeColor = System.Drawing.Color.Red;
+            this.error.Location = new System.Drawing.Point(179, 37);
+            this.error.Name = "error";
+            this.error.Size = new System.Drawing.Size(109, 17);
+            this.error.TabIndex = 4;
+            this.error.Text = "Wrong File Path";
+            this.error.Visible = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.RestoreDirectory = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.WindowFrame;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 115);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.RowTemplate.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ShowRowErrors = false;
+            this.dataGridView1.Size = new System.Drawing.Size(365, 655);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(663, 782);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.error);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.filePath);
+            this.Controls.Add(this.Run);
+            this.Controls.Add(this.Browse);
+            this.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.Name = "Form1";
+            this.Text = "Scanner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Browse;
+        private System.Windows.Forms.TextBox filePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label error;
+        private System.Windows.Forms.Button Run;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Compiler Scanner C#/Scanner/Scanner/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scanner
+{
+    public partial class Form1 : Form
+    {
+        DataTable T = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+            T.Columns.Add("Token Value", typeof(string));
+            T.Columns.Add("Token Type", typeof(string));
+            dataGridView1.DataSource = T;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        OpenFileDialog ofd = new OpenFileDialog();
+        private void Browse_Click(object sender, EventArgs e)
+        {
+           
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                filePath.Text = Global.path =ofd.FileName;
+
+            }
+        }
+        
+        private void Run_Click(object sender, EventArgs e)
+        {
+            
+            T.Rows.Clear();
+            dataGridView1.DataSource = T;
+             if (!File.Exists(filePath.Text)|Path.GetExtension(filePath.Text)!=".txt") error.Visible = true;
+            else
+            {
+                error.Visible=false ;
+                Global.path =filePath.Text;
+                scanner.scanFile();
+                string outputPathFile = (Path.GetDirectoryName(Global.path) + "\\scanner_output.txt");
+                System.IO.StreamWriter file = new System.IO.StreamWriter(@outputPathFile);
+                while (!Global.done)
+                {
+                    scanner.getNextToken();
+                    if (!Global.done)
+                    {
+                        T.Rows.Add(Global.tokenValue, Global.tokenType);
+                        dataGridView1.DataSource = T;
+                        file.WriteLine ("   "+Global.tokenValue+"    :    "+ Global.tokenType);
+                    }
+                }
+                file.Close();
+
+            }
+        }
+
+
+    }
+    public class scanner
+    {
+        public static void getNextToken ()
+        {
+
+            int currentState = 0;
+            /////////////////////////////////////////////////////////////////////////////////////
+            while (Global.code.Length > Global.nextToken)
+            {
+                char c = Global.code[Global.nextToken];
+                if (currentState == 0) // start state // this function does not handle any error at the beginning of the Token //
+                {
+                    if (char.IsDigit(c)) { currentState = 2; Global.tokenValue = c.ToString(); Global.tokenType = "Number"; }
+                    else if (char.IsLetter(c)) { currentState = 3; Global.tokenValue = c.ToString(); Global.tokenType = "Identifier"; }
+                    else if (c == ':') { currentState = 4; Global.tokenValue = c.ToString(); Global.tokenType = "Special Symbol"; }
+                    else if (c == '+' | c == '-' | c == '*' | c == '/' | c == '<' | c == ';' | c == '=' | c == '(' | c == ')') {  Global.tokenValue = c.ToString(); Global.tokenType = "Special Symbol"; Global.nextToken++;return; }
+                    else if (c == '{') { currentState = 1;}
+                    
+                }
+                
+                else if(currentState == 1) // Comment
+                {
+                    if(c=='}')  currentState = 0;
+                }
+                else if (currentState == 2) // integer number
+                {
+                    if (char.IsDigit(c)) Global.tokenValue += c.ToString();
+                    else if (c == '.')
+                    {
+                        Global.tokenValue += c.ToString();
+                        currentState = 5;
+                    }
+                    else return; 
+                }
+                else if (currentState == 3) // Identifier
+                {
+                    if (char.IsDigit(c) | char.IsLetter(c) | c == '_') Global.tokenValue += c.ToString();
+                    else
+                    {
+                        if (Global.tokenValue == "if" | Global.tokenValue == "then" | Global.tokenValue == "else" | Global.tokenValue == "end" | Global.tokenValue == "repeat" | Global.tokenValue == "until" | Global.tokenValue == "read" | Global.tokenValue == "write") Global.tokenType = "Reserved Words"; // IMPLEMENT THE RESERVED WORDS
+                        return;
+                    }
+                }
+                else if (currentState == 4) // Assignment
+                {
+                    if (c == '=') { Global.tokenValue += c.ToString(); Global.nextToken++; return; }
+                    else currentState = 0; // when he finds ':'
+                }
+                else if (currentState == 5) // Decimal point
+                {
+                    if (char.IsDigit(c)) { Global.tokenValue += c.ToString(); currentState = 6; }
+                    else currentState = 0;
+                    
+                }
+                else if (currentState == 6) // Decimal number
+                {
+                    if (char.IsDigit(c)) { Global.tokenValue += c.ToString(); }
+                    else return;
+                }
+               
+                Global.nextToken++;
+            }
+
+            if(Global.code.Length <= Global.nextToken)
+            {
+                Global.done = true;
+            }
+            else
+            {
+                Global.done = false;
+            }
+
+            
+        }
+        public static void scanFile()
+        {
+            Global.code = System.IO.File.ReadAllText(@Global.path)+" ";
+            Global.nextToken = 0;
+            Global.done = false;
+        }
+    }
+    static class Global
+    {
+        public static int nextToken = 0;// pointer to the next token
+        public static string code = ""; // line read to scan
+        public static string path = ""; //path of the file
+        public static string tokenValue=""; // contain the token value to be displayed
+        public static string tokenType=""; // contain the token type to be displayed
+        public static bool done = false;
+    }
+}
